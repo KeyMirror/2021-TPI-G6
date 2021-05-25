8358b0e9d92e8fb10103a8b1fb5f18aa852cd26e
Luis Gonzalo Cao <luisgonzalocao@gmail.com>
2021-05-25 19:46:35 -0300

    app register changes, and routes fixeds.

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const notificationRouter = require('./routes/notification.routes'); 
+// express app
+const app = express();
 
-var app = express();
+// routes
+const indexRouter = require('./routes/index');
+const notificationsRouter = require('./routes/notifications.routes'); 
 
+// uses routes
+app.use('/', indexRouter);
+app.use('/notifications', notificationsRouter);
+
+// uses modules
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/nofication', notificationRouter); 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const { Response } = require('../utils/'); 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const data = {
+    path: 'index "/" endpoint',
+    name: 'API Secretaria',
+    status: 'On-line',
+  }
+  Response.success(res, data)
 });
 
 module.exports = router;

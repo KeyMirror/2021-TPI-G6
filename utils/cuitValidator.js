f8968c3f25d233f0db303f49ef7d9720c848fcb5
Luis Gonzalo Cao <luisgonzalocao@gmail.com>
2021-06-06 02:05:58 -0300

    fixed cuitValidator

--- utils/cuitValidator.js
+++ utils/cuitValidator.js
@@ -2,9 +2,6 @@ const cuitValidator = (cuit) =>{
 
     const acceptValues = /^[0-9]+$/;
 
-    console.log(cuit.length)
-    console.log(cuit.match(acceptValues))
- 
     if (cuit.length !== 11 || !cuit.match(acceptValues)) {
         return false;
     }
